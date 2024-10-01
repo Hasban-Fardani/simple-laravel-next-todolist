cfbd4df39ee118f34e77bb77d8b4b09d10f3beff
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-10-01 09:35:17 +0700

    feat(route): setup api route and controller

---
+++ backend/app/Http/Controllers/TodoController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created todolist in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified todolist.
+     */
+    public function show(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified todolist in storage.
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified todolist from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        //
+    }
+}
