7b2e76f79bd4dfa7d733adf4943800f28191bcb9
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-10-01 09:36:25 +0700

    feat(migration): create migration

--- backend/database/migrations/2024_10_01_023142_create_todos_table.php
+++ backend/database/migrations/2024_10_01_023142_create_todos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->boolean('is_completed')->default(false);
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
