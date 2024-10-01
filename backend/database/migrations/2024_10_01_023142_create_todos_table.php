46dff64a55be217ade82cea9e67aa559de14e3e8
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-10-01 09:41:01 +0700

    feat(seeder): create todolist seeder

--- backend/database/migrations/2024_10_01_023142_create_todos_table.php
+++ backend/database/migrations/2024_10_01_023142_create_todos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
             $table->boolean('is_completed')->default(false);
             $table->text('description')->nullable();
             $table->timestamps();
--- backend/database/seeders/TodoSeeder.php
+++ backend/database/seeders/TodoSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Todo;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,38 @@ class TodoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $todos = [
+            [
+                'title' => 'Belajar dasar API',
+                'is_completed' => true,
+                'created_at' => now(),
+            ],
+
+            [
+                'title' => 'Belajar dasar Routing API di Laravel',
+                'is_completed' => true,
+                'created_at' => now(),
+            ],
+
+            [
+                'title' => 'Belajar dasar Routing di next js',
+                'is_completed' => true,
+                'created_at' => now(),
+            ],
+
+            [
+                'title' => 'Belajar dasar CRUD menggunakan next dan laravel',
+                'is_completed' => false,
+                'created_at' => now(),
+            ],
+
+            [
+                'title' => 'Belajar dasar Auth di laravel dan Next js',
+                'is_completed' => false,
+                'created_at' => now(),
+            ],
+        ];
+
+        Todo::insert($todos);
     }
 }
